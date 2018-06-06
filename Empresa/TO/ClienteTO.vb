ae8e487bf097a86c60e3c234a250bbd82dfe5269
AnPLM <patricia_2897@hotmail.com>
2018-06-05 22:56:27 -0600

    Con TO y BL's

---
+++ Empresa/TO/ClienteTO.vb
@@ -0,0 +1,9 @@
+﻿Public Class ClienteTO
+
+    Public Property Cedula As String
+    Public Property Nombre As String
+    Public Property Apellido As String
+    Public Property Correo As String
+    Public Property Telefono As Integer
+
+End Class
